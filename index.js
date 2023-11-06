2308070757b7e4a4e65c995b0bc4c052924c81c1
Vitor Ciscoto <vitor.ciscoto@markway.com>
2023-11-06 14:55:42 -0300

    correção nome do arquivo

---
+++ index.js
@@ -0,0 +1,32 @@
+let nome = "Vitor";
+let xp = 2000;
+let nivel;
+
+switch (true) {
+    case xp < 1000:
+        nivel = "Ferro";
+        break;
+    case xp >= 1000 && xp <= 2000:
+        nivel = "Bronze";
+        break;
+    case xp > 2000 && xp <= 5000:
+        nivel = "Prata";
+        break;
+    case xp > 5000 && xp <= 7000:
+        nivel = "Ouro";
+        break;
+    case xp > 7000 && xp <= 8000:
+        nivel = "Platina";
+        break;
+    case xp > 8000 && xp <= 9000:
+        nivel = "Ascendente";
+        break;
+    case xp > 9000 && xp <= 10000:
+        nivel = "Imortal";
+        break;
+    case xp > 10000:
+        nivel = "Radiante";
+        break;
+}
+
+console.log("O Herói de nome " + `**${nome}**` + " está no nível de **" + nivel + "**");
